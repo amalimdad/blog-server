128dee66caa6f2360e27abe94f95227c83fd3541
Amal Saleem <amals.imdad@gmail.com>
2022-08-10 01:04:38 +0300

    apply ssr

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import fs from 'fs';
+import path from 'path'
+import React from 'react';
+import ReactDomServer from 'react-dom/server';
+import App from "../src/pages/App"
+
+const PORT = 8000;
+
+const app = express();
+
+app.use('^$', (request, response, next) => {
+  fs.readFile(path.resolve('./build/index.html', 'utf-8', (err, data) => {
+    if (err) {
+      console.log(err);
+      return response.status(500).send('some error happened')
+    }
+    return response.send(data.replace('<div id="root"></div>', `<div id="root">${ReactDomServer.renderToString(<App />)}</div>`))
+  }))
+})
+
+app.use(express.static((path.resolve(__dirname, '..', 'build'))))
+app.listen(PORT, () => {
+  console.log(`App lunched on ${PORT}`);
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.scss';
 import App from "./pages/App"
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
+// const root = 
+ReactDOM.hydrate(<App />,
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
